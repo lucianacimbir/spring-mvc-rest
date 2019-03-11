75caa0b320fae0bdb990f40ab9879580e3e6e5c4
lucianacimbir <lucianacimbir@gmail.com>
2019-03-11 15:43:21 +0200

    Added delete functionality and fixed create

--- src/main/java/spring/backend/springmvcrest/services/FavoriteService.java
+++ src/main/java/spring/backend/springmvcrest/services/FavoriteService.java
@@ -1,11 +1,10 @@
 package spring.backend.springmvcrest.services;
 
-import spring.backend.springmvcrest.model.ApplicationUser;
-import spring.backend.springmvcrest.model.Spot;
+import spring.backend.springmvcrest.model.Favorite;
 
 public interface FavoriteService {
 
-    ApplicationUser create(Long user_id, Long spot_id);
+    Favorite create(Long user_id, Long spot_id);
 
     void delete(Long user_id, Long spot_id);
 }
--- src/main/java/spring/backend/springmvcrest/services/FavoriteServiceImpl.java
+++ src/main/java/spring/backend/springmvcrest/services/FavoriteServiceImpl.java
@@ -2,48 +2,42 @@ package spring.backend.springmvcrest.services;
 
 import org.springframework.stereotype.Service;
 import spring.backend.springmvcrest.model.ApplicationUser;
+import spring.backend.springmvcrest.model.Favorite;
 import spring.backend.springmvcrest.model.Spot;
-import spring.backend.springmvcrest.repositories.SpotRepository;
-import spring.backend.springmvcrest.repositories.UserRepository;
+import spring.backend.springmvcrest.repositories.FavoriteRepository;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 @Service
 public class FavoriteServiceImpl implements FavoriteService {
 
-    private final UserRepository userRepository;
-    private final SpotRepository spotRepository;
+    private final FavoriteRepository favoriteRepository;
+    private final EntityManager em;
 
-    public FavoriteServiceImpl(UserRepository userRepository, SpotRepository spotRepository) {
-        this.userRepository = userRepository;
-        this.spotRepository = spotRepository;
-        System.out.println("dwadwadwadwa");
-        System.out.println(this.userRepository);
-    }
-
-
-    @Override
-    public ApplicationUser create(Long user_id, Long spot_id) {
-        System.out.println("balalalalala");
-        System.out.println(user_id);
-        System.out.println(spot_id);
-        System.out.println(this.userRepository);
-        System.out.println("balalalalala");
-        ApplicationUser user = userRepository.findById(user_id).get();
-
-        System.out.println(user);
-        Spot spot = spotRepository.findById(spot_id).get();
-
-        //System.out.println(spot);
-
-        user.getSpots().add(spot);
-        //spot.getUsers().add(user);
-        userRepository.save(user);
-        //spotRepository.save(spot);
-
-        return user;
+    public FavoriteServiceImpl(FavoriteRepository favoriteRepository, EntityManager em) {
+        this.favoriteRepository = favoriteRepository;
+        this.em = em;
     }
 
     @Override
+    public Favorite create(Long user_id, Long spot_id) {
+
+        ApplicationUser applicationUser = em.getReference(ApplicationUser.class, user_id);
+        Spot spot = em.getReference(Spot.class, spot_id);
+        Favorite favorite = new Favorite();
+        favorite.setUser(applicationUser);
+        favorite.setSpot(spot);
+        favoriteRepository.save(favorite);
+
+        return favorite;
+    }
+
+    @Override
+    @Transactional
     public void delete(Long user_id, Long spot_id) {
+
+        Integer ok = favoriteRepository.deleteByUserIdAndSpotId(user_id, spot_id);
 
     }
 }
